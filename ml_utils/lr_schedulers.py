c52eb8cdecc3bba2551b06e9e6178b9c9d1d7cc7
cameron-git <cameronbarkeran@gmail.com>
2025-05-04 18:52:47 +0100

    more lr schedulers

--- ml_utils/lr_schedulers.py
+++ ml_utils/lr_schedulers.py
@@ -1,17 +1,18 @@
 import math
 from functools import partial
+from typing import Callable
 import torch
 
 __all__ = ["get_lr_scheduler"]
 
 
-def _get_cosine_schedule_with_warmup_lr_lambda(
+def cosine_lr_lambda(
     current_step: int,
     *,
-    num_warmup_steps: int,
     num_training_steps: int,
-    num_cycles: float,
-    min_lr_rate: float = 0.0,
+    num_warmup_steps: int = 0,
+    num_cycles: float = 0.5,
+    min_lr_rate: float = 0.1,
 ):
     if current_step < num_warmup_steps:
         return float(current_step) / float(max(1, num_warmup_steps))
@@ -23,24 +24,92 @@ def _get_cosine_schedule_with_warmup_lr_lambda(
     return max(0, factor)
 
 
+def linear_lr_lambda(
+    current_step: int,
+    *,
+    num_training_steps: int,
+    num_warmup_steps: int = 0,
+    min_lr_rate: float = 0.0,
+):
+    if current_step < num_warmup_steps:
+        return float(current_step) / float(max(1, num_warmup_steps))
+    progress = float(current_step - num_warmup_steps) / float(
+        max(1, num_training_steps - num_warmup_steps)
+    )
+    factor = 1.0 - progress
+    factor = factor * (1 - min_lr_rate) + min_lr_rate
+    return max(0, factor)
+
+
+def constant_2_linear_lr_lambda(
+    current_step: int,
+    *,
+    num_training_steps: int,
+    num_warmup_steps: int = 0,
+    min_lr_rate: float = 0.0,
+    phase_ratio: float = 0.8,
+    num_2nd_warmup_steps: int = 0,
+):
+    if current_step < num_warmup_steps:
+        return float(current_step) / float(max(1, num_warmup_steps))
+    if current_step < num_training_steps * phase_ratio:
+        return 1.0
+    if current_step < num_training_steps * phase_ratio + num_2nd_warmup_steps:
+        return float(current_step - num_training_steps * phase_ratio) / float(
+            max(1, num_2nd_warmup_steps)
+        )
+    progress = float(
+        current_step - num_training_steps * phase_ratio - num_2nd_warmup_steps
+    ) / float(
+        max(
+            1,
+            num_training_steps
+            - num_training_steps * phase_ratio
+            - num_2nd_warmup_steps,
+        )
+    )
+    factor = 1.0 - progress
+    factor = factor * (1 - min_lr_rate) + min_lr_rate
+    return max(0, factor)
+
+
 def get_lr_scheduler(
     optimizer: torch.optim.Optimizer,
-    train_steps: int,
-    warmup_steps: int = 0,
     scheduler_type: str = "cosine",
+    lr_lambda: Callable = None,
+    **kwargs,
 ):
-    if scheduler_type == "cosine":
+    """
+    scheduler_type: str, one of ["lambda", "cosine", "linear"]
+    kwargs:
+        num_training_steps: int,
+        num_warmup_steps: int,
+        min_lr_rate: float,
+        phase_ratio: float,
+        num_2nd_warmup_steps: int,
+    """
+    if scheduler_type == "lambda":
+        assert lr_lambda is not None, "lr_lambda must be provided for lambda scheduler"
+        lr_lambda = lr_lambda
+    elif scheduler_type == "cosine":
         lr_lambda = partial(
-            _get_cosine_schedule_with_warmup_lr_lambda,
-            num_warmup_steps=warmup_steps,
-            num_training_steps=train_steps,
-            num_cycles=0.5,
-            min_lr_rate=0.1,
+            cosine_lr_lambda,
+            **kwargs,
         )
-        lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
-            optimizer,
-            lr_lambda,
+    elif scheduler_type == "linear":
+        lr_lambda = partial(
+            linear_lr_lambda,
+            **kwargs,
+        )
+    elif scheduler_type == "constant_2_linear":
+        lr_lambda = partial(
+            constant_2_linear_lr_lambda,
+            **kwargs,
         )
     else:
         raise ValueError(f"Unknown scheduler type {scheduler_type}")
+    lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer,
+        lr_lambda,
+    )
     return lr_scheduler
